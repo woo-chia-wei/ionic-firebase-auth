3fa5850b755440cc6e344f3c02f131b9653bcf7e
Woo Chia Wei <chiaweiwoo123@gmail.com>
2018-04-01 21:48:30 +0800

    Removed environment.ts from git.

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBHhUCCHW-br83ZINBTIAzyO-rkKwvt0xY",
-    authDomain: "ionic-auth-1d649.firebaseapp.com",
-    databaseURL:  "https://ionic-auth-1d649.firebaseio.com",
-    projectId: "ionic-auth-1d649",
-    storageBucket: "ionic-auth-1d649.appspot.com",
-    messagingSenderId: "38734180717"
-  };
